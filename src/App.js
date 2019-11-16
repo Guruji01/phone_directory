43fbd59fcfd1d19cde228dcbd9a6770b4c7e7b88
Admin <naveenkaushal441@gmail.com>
2019-11-16 20:56:04 +0530

    App.index file updated 4....

--- src/App.js
+++ src/App.js
@@ -3,29 +3,11 @@ import React, {Component} from 'react';
 class App extends Component
 {
 render() {
-  let x = 2;
-  let y = 4;
+
   return (
- <div>
-    {/*<div>
-         Phone Directory
-    </div>
 
-    <button> Clic me ! </button>
-      
-    <div>
-        <span> Name</span><br/>
-        <span> Phone </span>
-    </div> */}
-  
-
-    <lable htmlFor = "name">Name</lable>
-    <input id="name" type = "text" placeholder="Type Here"
-     defaultValue ="Jack"/> <br/>
-      
-        <input id="name" type = "text" placeholder="Type Here"
-        defaultValue ={x+y}/>
-          </div> 
+   React.createElement("div", {id: "module"},
+   React.createElement("p",null,"ReactJs"))
   );
 }
 }
