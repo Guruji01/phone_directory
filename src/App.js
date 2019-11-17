93814c6624a384326820f977589266490b0ad795
Admin <naveenkaushal441@gmail.com>
2019-11-17 12:41:34 +0530

    delete buttin added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let subscribers = [
+    {
+      id: 1,
+      name: "Pihu",
+      phone: "8219432884"
+    },
+    {
+      id: 2,
+      name: "Jack",
+      phone: "9017924441"
+    }
+  ]
     return (
-      <div>
+      <div className="component-container">
           <Header />
           <div className="component-body-container">
-
           <button className="custom-btn add-btn">Add</button>
 
           <div className="grid-container heading-container">
@@ -16,17 +27,17 @@ class App extends Component {
           <span className="grid-item phone-heading">Phone</span>
           </div>
 
-          <div className="grid-container">
-          <span className="grid-item">Pihu</span>
-          <span className="grid-item">8219432884</span>
-          </div>
-
-          <div className="grid-container">
-          <span className="grid-item">Jack</span>
-          <span className="grid-item">9017924441</span>
-          </div>
-
-
+      {
+        subscribers.map(sub => {
+        return <div key={sub.id} className="grid-container">
+        <span className="grid-item">{sub.name}</span>
+        <span className="grid-item">{sub.phone}</span>
+        <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+        </div>
+        })
+      }
           </div>
           </div>
     );
