cd188da689c333c281feafb7a53b48820b884d61
Admin <naveenkaushal441@gmail.com>
2019-11-17 12:13:49 +0530

    App.js file updated

--- src/App.js
+++ src/App.js
@@ -10,10 +10,23 @@ class App extends Component {
           <div className="component-body-container">
 
           <button className="custom-btn add-btn">Add</button>
+
           <div className="grid-container heading-container">
           <span className="grid-item name-heading">Name</span>
           <span className="grid-item phone-heading">Phone</span>
           </div>
+
+          <div className="grid-container">
+          <span className="grid-item">Pihu</span>
+          <span className="grid-item">8219432884</span>
+          </div>
+
+          <div className="grid-container">
+          <span className="grid-item">Jack</span>
+          <span className="grid-item">9017924441</span>
+          </div>
+
+
           </div>
           </div>
     );
